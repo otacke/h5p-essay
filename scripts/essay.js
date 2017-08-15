f44ebee7dc26e515171320f1985f2b544e82ef5e
Oliver Tacke <o.tacke@posteo.de>
2017-08-15 20:07:46 +0200

    add computeScore method

--- scripts/essay.js
+++ scripts/essay.js
@@ -49,14 +49,23 @@ H5P.Essay = function ($) {
     }
 
     // Text Area
-    // TODO: Check if we can easily use h5p-text-input-field
+    // TODO: Check if we can easily use and modify h5p-text-input-field
     this.text = document.createElement('textarea');
     this.text.classList.add('h5p-essay-text');
     this.text.setAttribute('tabindex', 0);
     this.text.setAttribute('required', '');
     this.text.setAttribute('rows', 10);
-    this.text.innerHTML = 'textarea';
+    //this.text.innerHTML = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
     $wrapper.get(0).appendChild(this.text);
+
+    // Check-Button
+    // TODO: Check how the Check-Button + Score Bar is used in H5P contents
+    this.buttonCheck = document.createElement('button');
+    this.buttonCheck.innerHTML = 'CHECK';
+    this.buttonCheck.addEventListener('click', function() {
+      console.log(that.computeScore());
+    });
+    $wrapper.get(0).appendChild(this.buttonCheck);
 
     that.trigger('resize');
   };
@@ -68,6 +77,21 @@ H5P.Essay = function ($) {
   // Extends the event dispatcher
   Essay.prototype = Object.create(H5P.EventDispatcher.prototype);
   Essay.prototype.constructor = Essay;
+
+  Essay.prototype = {
+    computeScore: function() {
+      var result = 0;
+      // Remove everything but characters, numbers and spaces
+      // TODO: Think if leaving - might be advisable
+      var words = this.text.value.replace(/[^A-Za-z0-9\s]/g,'').split(' ');
+      for (var i = 0; i < this.config.keywords.length; i++) {
+        if (words.indexOf(this.config.keywords[i]) !== -1) {
+          result++;
+        }
+      }
+      return result;
+    }
+  };
 
   return Essay;
 }(H5P.jQuery);
