d3a35013504946e8e0fee9c83d8bf07984aa3aec
Oliver Tacke <o.tacke@posteo.de>
2017-08-19 18:36:34 +0200

    use JoubelUI and Question for button

--- scripts/essay.js
+++ scripts/essay.js
@@ -1,6 +1,6 @@
 var H5P = H5P || {};
 
-H5P.Essay = function ($) {
+H5P.Essay = function ($, Question) {
   'use strict';
   /**
    * @constructor
@@ -14,69 +14,79 @@ H5P.Essay = function ($) {
     if (!config) {
       return;
     }
+
+    // Inheritance
+    Question.call(this, 'essay');
+
     this.config = config;
     this.contentId = contentId;
     this.contentData = contentData || {};
 
     // Create DOM
     this.element = document.createElement('div');
-
-    /**
-     * Attach function called by H5P framework to insert H5P content into page.
-     * TODO: Remove this jQuery dependency as soon as the H5P framework is ready
-     *
-     * @param {jQuery} $wrapper - Container to attach to.
-     */
-    this.attach = function($wrapper) {
-      var that = this;
-      $wrapper.get(0).classList.add('h5p-essay');
-
-    // Title
-    if (that.config.title) {
-      var title = document.createElement('div');
-      title.classList.add('h5p-essay-title');
-      title.innerHTML = '<h2>' + that.config.title + '</h2>';
-      $wrapper.get(0).appendChild(title);
-    }
-
-    // Inputfield
-    this.inputfield = new H5P.TextInputField(this.config.inputfield.params, this.contentId);
-    this.inputfield.attach($wrapper);
-
-    // Check-Button
-    // TODO: Check how the Check-Button + Score Bar is used in H5P contents
-    this.buttonCheck = document.createElement('button');
-    this.buttonCheck.innerHTML = 'CHECK';
-    this.buttonCheck.addEventListener('click', function() {
-      console.log(that.computeScore());
-    });
-    $wrapper.get(0).appendChild(this.buttonCheck);
-    
-    that.trigger('resize');
-  };
-
-    // Initialize event inheritance
-    H5P.EventDispatcher.call(this);
   }
 
-  // Extends the event dispatcher
-  Essay.prototype = Object.create(H5P.EventDispatcher.prototype);
+  // Extends Question
+  Essay.prototype = Object.create(Question.prototype);
   Essay.prototype.constructor = Essay;
 
   Essay.prototype = {
     computeScore: function() {
       var result = 0;
-      // Remove everything but characters, numbers and spaces
-      // TODO: Think if leaving - might be advisable
-      var words = this.inputfield.getInput().value.replace(/[^A-Za-z0-9\s]/g,'').split(' ');
+      // Remove everything but characters, numbers, - and spaces
+      var words = this.inputfield.getInput().value
+        .replace(/(\r\n|\r|\n)/g, ' ')
+        .replace(/[^A-Za-z0-9-\s]/g,'')
+        .split(' ');
       for (var i = 0; i < this.config.keywords.length; i++) {
         if (words.indexOf(this.config.keywords[i]) !== -1) {
           result++;
         }
       }
       return result;
+    },
+    registerDomElements: function() {
+      var that = this;
+
+      /*
+       * This is a little ugly for several reasons. First of all, it would be
+       * nicer if H5P.TextInputField had a function to return the HTML for
+       * introduction and content. However, this way we don't have to touch that
+       * code right now. Secondly, we use jQuery. H5P is going to ditch it, but
+       * we can use the existing attach method.
+       * TODO: Get rid of jQuery and change H5P.TextInputField as soon as the
+       *       H5P core runs without jQuery.
+       */
+      var $wrapper_inputfield = $('<div>');
+      that.inputfield = new H5P.TextInputField(this.config.inputfield.params, this.contentId);
+      that.inputfield.attach($wrapper_inputfield);
+
+      // Register task introduction text
+      that.setIntroduction($wrapper_inputfield.children().get(0));
+
+      // Register content
+      that.setContent($wrapper_inputfield.get(0));
+
+      // Register Buttons
+      this.addButtons();
+    },
+    addButtons: function () {
+      var that = this;
+
+      // Check answer button
+      that.addButton('check-answer', that.config.checkAnswer, function () {
+        that.showEvaluation();
+      }, true, {}, {});
+    },
+    showEvaluation: function () {
+      var that = this;
+      that.hideButton('check-answer');
+      this.setFeedback(
+        'Let us see how many of my keywords you used ...',
+        that.computeScore(),
+        that.config.keywords.length);
     }
   };
 
   return Essay;
-}(H5P.jQuery);
+}(H5P.jQuery, H5P.Question);
